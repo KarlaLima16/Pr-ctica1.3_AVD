07967736836c3c17f2eccbb93f0b2d2d2cb72475
DidierGarciaToquiantzi <124199143+DidierGarciaToquiantzi@users.noreply.github.com>
2023-09-25 23:50:27 -0600

    Modificacion_D

--- medidas-similitud.py
+++ medidas-similitud.py
@@ -17,3 +17,31 @@ tfidf_matrix = tfidf_vectorizer.fit_transform(data['combined_features'])
 
 # Calcula la similitud coseno entre las películas
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+movie_index = 1
+
+# Calcula las puntuaciones de similitud coseno para la película seleccionada
+cosine_scores = list(enumerate(cosine_sim[movie_index]))
+
+# Ordena las películas según la similitud coseno en orden descendente
+cosine_scores = sorted(cosine_scores, key=lambda x: x[1], reverse=True)
+
+# Calcula las puntuaciones de similitud Jaccard para la película seleccionada
+jaccard_scores = list(enumerate(jaccard_sim[movie_index]))
+
+# Ordena las películas según la similitud Jaccard en orden descendente
+jaccard_scores = sorted(jaccard_scores, key=lambda x: x[1], reverse=True)
+
+# Muestra las 5 mejores recomendaciones basadas en similitud coseno
+top_coseno_recomendador = cosine_scores[1:6]
+
+# Muestra las 5 mejores recomendaciones basadas en similitud Jaccard
+top_jaccard_recomendador = jaccard_scores[1:6]
+
+# muestra las recomendaciones
+print("Recomendaciones basadas en similitud coseno:")
+for i, score in top_coseno_recomendador:
+    print(data.iloc[i]['Title'], data.iloc[i]['Genres'], data.iloc[i]['Actor1'])
+
+print("\nRecomendaciones basadas en similitud Jaccard:")
+for i, score in top_jaccard_recomendador:
+    print(data.iloc[i]['Title'], data.iloc[i]['Genres'], data.iloc[i]['Actor1'])
